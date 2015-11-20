a801393ab7e2c101e17a392bbfd5bdb8a1c6c255
Michael Spallino <mspallino@wit.edu>
2015-11-20 13:58:25 -0500

    Function memory!

--- lang/Interpreter.py
+++ lang/Interpreter.py
@@ -6,6 +6,7 @@ class Interpreter(ParseTreeVisitor):
     results = []
     memory = {}
     function_table = {}
+    context = memory
 
     def visitProg(self, ctx):
         i = 0
@@ -30,10 +31,10 @@ class Interpreter(ParseTreeVisitor):
         if expr.number:
             return float(expr.number.text)
         if expr.ident:
-            if expr.ident.text in self.memory.keys():
-                return self.memory[expr.ident.text]
+            if expr.ident.text in self.context.keys():
+                return self.context[expr.ident.text]
             else:
-                print(self.memory)
+                print(self.context)
                 raise Exception('Variable: {val} not defined.'.format(val=expr.ident.text))
         if expr.sub:
             return self.visit(expr.sub)
@@ -54,27 +55,39 @@ class Interpreter(ParseTreeVisitor):
         return 0.0
 
     def visitFuncDef(self, ctx):
+        func_mem = {}
         ident = ctx.ident.text
         params = ctx.params
         block = ctx.bl
-        self.function_table[ident] = (self.visit(params), block)
+        self.function_table[ident] = (self.visit(params), func_mem, block, )
 
     def visitFuncCall(self, ctx):
         ident = ctx.ident.text
         if ident in self.function_table.keys():
             params_list = self.visit(ctx.params)
+            self.context = self.function_table[ident][1]
+            for param in params_list:
+                param = param.strip()
+                self.context[param] = self.memory[param]
+            for val in self.memory.keys():
+                if val not in self.context.keys():
+                    self.context[val] = self.memory[val]
             if params_list == self.function_table[ident][0]:
-                return self.visit(self.function_table[ident][1])
+                return_val = self.visit(self.function_table[ident][2])
+                self.context = self.memory
+                return return_val
             else:
+                self.context = self.memory
                 raise Exception('Incorrect parameters for {func} ({params})'.format(func=ident, params=params_list))
+
         else:
             raise Exception('Function {func} not defined.'.format(func=ident))
 
     def visitRetrn(self, ctx):
-        if ctx.value.text in self.memory.keys() and not(ctx.value.text[0].isdigit()):
-            return self.memory[ctx.value.text]
-        elif ctx.value.text in self.memory.keys() and ctx.value.text[0].isdigit():
-            print(self.memory)
+        if ctx.value.text in self.context.keys() and not(ctx.value.text[0].isdigit()):
+            return self.context[ctx.value.text]
+        elif ctx.value.text in self.context.keys() and ctx.value.text[0].isdigit():
+            print(self.context)
             raise Exception('Variable: {val} not defined.'.format(val=ctx.value.text))
         else:
             return ctx.value.text
@@ -83,9 +96,8 @@ class Interpreter(ParseTreeVisitor):
         return ''.join([stmt.symbol.text for stmt in ctx.children]).split(',')
 
     def visitDecl(self, ctx):
-        self.memory[ctx.ident.text] = self.visit(ctx.right)
+        self.context[ctx.ident.text] = self.visit(ctx.right)
 
-    # FIX LATER
     def visitBlock(self, ctx):
         for stmt in ctx.children:
             if isinstance(stmt, ExprParser.RetrnContext):
@@ -96,7 +108,7 @@ class Interpreter(ParseTreeVisitor):
         return 0
 
     def visitPrintc(self, ctx):
-        if ctx.ident.text in self.memory.keys():
-            print(self.memory[ctx.ident.text])
+        if ctx.ident.text in self.context.keys():
+            print(self.context[ctx.ident.text])
         else:
             raise Exception('Variable: {val} not defined.'.format(val=ctx.ident.text))
